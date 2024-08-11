2aeb2fa8cc60848ee96060803bbf47959e89899e
Naneynonn <oddy239@gmail.com>
2024-08-11 13:30:06 +0300

    update voice rest

--- src/Rest/Voice.php
+++ src/Rest/Voice.php
@@ -24,4 +24,46 @@ final class Voice
       cache_ttl: $cache_ttl
     );
   }
+
+  public function getCurrentUserVoiceState(string $guild_id, ?int $cache_ttl = 600): array
+  {
+    return $this->api->request(
+      method: RequestTypes::GET,
+      endpoint: "guilds/{$guild_id}/voice-states/@me",
+      cache_ttl: $cache_ttl
+    );
+  }
+
+  public function getUserVoiceState(string $guild_id, string $user_id, ?int $cache_ttl = 600): array
+  {
+    return $this->api->request(
+      method: RequestTypes::GET,
+      endpoint: "guilds/{$guild_id}/voice-states/{$user_id}",
+      cache_ttl: $cache_ttl
+    );
+  }
+
+  public function modifyCurrentUserVoiceState(string $guild_id, array $params = [], ?int $cache_ttl = 600): array
+  {
+    return $this->api->request(
+      method: RequestTypes::PATCH,
+      endpoint: "guilds/{$guild_id}/voice-states/@me",
+      options: [
+        'json' => $params
+      ],
+      cache_ttl: $cache_ttl
+    );
+  }
+
+  public function modifyUserVoiceState(string $guild_id, string $user_id, array $params = [], ?int $cache_ttl = 600): array
+  {
+    return $this->api->request(
+      method: RequestTypes::PATCH,
+      endpoint: "guilds/{$guild_id}/voice-states/{$user_id}",
+      options: [
+        'json' => $params
+      ],
+      cache_ttl: $cache_ttl
+    );
+  }
 }
